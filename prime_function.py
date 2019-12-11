d8a18401b529af356e91d5d61b92e625c909594e
aar-prog <aarush.pothula@gmail.com>
2019-12-11 20:56:38 +0100

    just added the comments

--- prime_function.py
+++ prime_function.py
@@ -1,14 +1,13 @@
+#function to check if the number is prime
 def is_prime(x):
     for a in range(2, x):
         if x%a==0:
             return False
         
     return True
-    
-    
-    
 
-for x in range(3,10000):
+#loop through number range
+for x in range(3,100):
     if is_prime(x):
         print(x)
         
