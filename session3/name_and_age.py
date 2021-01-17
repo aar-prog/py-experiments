d44133d63b1bddd8d7a0801b1b9d234de27b6d73
aar-prog <aarush.pothula@gmail.com>
2021-01-17 14:21:57 +0100

    changes to the name and age and added input and used for loop to print names and ages on one line

--- session3/name_and_age.py
+++ session3/name_and_age.py
@@ -1,23 +1,35 @@
 names=["Aarush", "Anjali", "Krishna", "Sreelatha"]
-ages=[12,9,42,38]
+ages=[9,6,35,32]
 rows=len(names)
-print(rows)
-name="Aarush"
-print(len(name))
-print(names[0],ages[0])
-print(names[1],ages[1])
-print(names[2],ages[2])
-print(names[3],ages[3])
+print(rows) #prints the number of the length of the list
 
-name="Aarush"
+print(names[0],"is",ages[0],"years old")
+print(names[1],"is",ages[1],"years old")
+print(names[2],"is",ages[2],"years old")
+print(names[3],"is",ages[3],"years old")
+print("-----------------------")
+
+#Looping thrugh the lists
+for a in names:
+    print(a)
+for b in ages:
+    print(b)
+print("-----------------------")
+#Loop through both lists same time
+for i in range(len(names)):
+    #print(i)
+    print(names[i],"is",ages[i],"years old")
+
+print("-----------------------")
+name=input("Enter your name:\n")
 print(name)
-print(len(name))
-print(name[0:3])
+print(len(name)) #prints the number of characters in my name
+print("These are the first 3 letters of my name:",name[0:3]) 
 print(name[::1])
 print(name[::2])
 print(name[::3])
 
-name="Aarush"
+#name="Aarush"
 for a in name:
     print(a)
 
